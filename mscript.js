e925107fafd37bc8d5e4dde69950b79d2acfb4d4
mbytemak <34517148+mbytemak@users.noreply.github.com>
2018-05-28 15:47:18 +0530

    Add files via upload

---
+++ mscript.js
@@ -0,0 +1,19 @@
+ $(document).ready(function() {
+
+   $("#idbutton").click(()=>{
+     
+   
+   $(".modal").css("display","block");
+   });
+   $(".close-btn").click(()=>{
+     
+   
+   $(".modal").css("display","none");
+     
+   });
+   
+   
+   
+   
+   
+});
